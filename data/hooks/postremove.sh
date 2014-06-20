cdfd72f06f032a81bd5fd9d81903dbcdc157cde1
Thomas Alrin <alrin@megam.co.in>
2014-06-20 20:48:46 +0530

    remove script

--- data/hooks/postremove.sh
+++ data/hooks/postremove.sh
@@ -1,4 +1,13 @@
 #!/usr/bin/env bash
 
 
+<% if after_remove && File.readable?(after_remove) %>
+# Call custom postremove script.
+CUSTOM_POSTREMOVE_SCRIPT="<%= Base64.encode64 File.read(after_remove) %>"
 
+tmpfile=$(mktemp)
+chmod a+x "${tmpfile}"
+echo "${CUSTOM_POSTREMOVE_SCRIPT}" | base64 -d - > ${tmpfile}
+
+"${tmpfile}" "$@"
+<% end %>
--- data/hooks/preremove.sh
+++ data/hooks/preremove.sh
@@ -1,3 +1,12 @@
 #!/usr/bin/env bash
 
+<% if before_remove && File.readable?(before_remove) %>
 
+CUSTOM_PREREMOVE_SCRIPT="<%= Base64.encode64 File.read(before_remove) %>"
+
+tmpfile=$(mktemp)
+chmod a+x "${tmpfile}"
+echo "${CUSTOM_PREREMOVE_SCRIPT}" | base64 -d - > ${tmpfile}
+
+"${tmpfile}" "$@"
+<% end %>

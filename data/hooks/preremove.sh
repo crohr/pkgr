ddd71944a9ab86f1a7ffe99f7ded2e9f6b19a77c
Thomas Alrin <alrin@megam.co.in>
2014-06-20 16:39:24 +0530

    remove fix

--- data/hooks/preremove.sh
+++ data/hooks/preremove.sh
@@ -1,29 +1,3 @@
 #!/usr/bin/env bash
 
-set -e
 
-export APP_NAME="<%= name %>"
-export APP_USER="<%= user %>"
-export APP_GROUP="<%= group %>"
-export APP_HOME="<%= home %>"
-
-if ! getent passwd "${APP_USER}" > /dev/null; then
-  if [ -f /etc/redhat-release ]; then
-    adduser "${APP_USER}" --user-group --system --create-home --shell /bin/bash
-  else
-    adduser "${APP_USER}" --disabled-login --group --system --quiet --shell /bin/bash
-  fi
-fi
-
-<% if before_remove && File.readable?(before_remove) %>
-# Call custom preremove script. The package will not yet be unpacked, so the
-# preinst script cannot rely on any files included in its package.
-# https://www.debian.org/doc/debian-policy/ch-maintainerscripts.html
-CUSTOM_PREREMOVE_SCRIPT="<%= Base64.encode64 File.read(before_remove) %>"
-
-tmpfile=$(mktemp)
-chmod a+x "${tmpfile}"
-echo "${CUSTOM_PREREMOVE_SCRIPT}" | base64 -d - > ${tmpfile}
-
-"${tmpfile}" "$@"
-<% end %>

9498606e7e808aed059ce4774c2006d1323caca5
Cyril Rohr <cyril.rohr@gmail.com>
2015-01-09 12:12:34 +0000

    Fix provision script

--- boxes/sles/12/bootstrap.sh
+++ boxes/sles/12/bootstrap.sh
@@ -16,7 +16,20 @@ zypper install -y ruby2.1 ruby2.1-devel git-core
 gem install bundler --no-ri --no-rdoc
 ln -fs /usr/bin/bundle.ruby2.1 /usr/bin/bundle
 
-cd /vagrant
-test -d "pkgr" || git clone https://github.com/crohr/pkgr.git
-cd pkgr
+zypper install -y postgresql-server
+echo "CREATE USER \"user\" SUPERUSER PASSWORD 'pass';" | su - postgres -c psql && \
+	echo "CREATE DATABASE dbname;" | su - postgres -c psql && \
+	echo "GRANT ALL PRIVILEGES ON DATABASE \"dbname\" TO \"user\";" | su - postgres -c psql
+
+cat > /var/lib/pgsql/data/pg_hba.conf <<CONFIG
+local   all             all                                     peer
+host    all             all             127.0.0.1/32            md5
+host    all             all             ::1/128                 md5
+CONFIG
+
+sed -i "s|#listen_addresses = 'localhost'|listen_addresses = 'localhost'|" /var/lib/pgsql/data/postgresql.conf
+
+systemctl restart postgresql
+
+cd /vagrant/pkgr
 bundle
